c7a38303e48b99456cbc0d3ce06ac3cac54bc30f
jo-ou <xuwang@basenet.co.jp>
2025-03-28 14:36:45 +0900

    develop commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -11,6 +11,6 @@ public class Main {
     public void doSomething(File file, Lock lock) {
         file.delete();  // Noncompliant
         // ...
-        // lock.tryLock(); // Noncompliant
+        lock.tryLock(); // Noncompliant
     }
 }
